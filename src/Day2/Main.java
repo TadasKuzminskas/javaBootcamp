0288f31cf8d59ce1a84b84d6acfe6a2e74524eea
Tadas Kuzminskas <Kuzminskas.tadas@gmail.com>
2022-02-15 11:15:59 +0200

    Initial commit of testFile

--- src/Day2/Main.java
+++ src/Day2/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //There are supposed to be changes
+        System.out.println("Testing branching and casting");
+
     }
 }
